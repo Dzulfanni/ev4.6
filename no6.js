1aa3bd4e1c3f56b710eb5345e9ccfbc6bd107252
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 22:24:56 +0700

    first commit

---
+++ no6.js
@@ -0,0 +1,13 @@
+mentorProgrammer(nama,status,asal) {
+    console.log('nama wandi pratama'
+)
+}
+status(nama){
+    console.log(statusnya santri)
+}
+
+mentorProgrammer(){
+    console.log(Berasal dari kota prabumulih)
+}
+
+console.log(mentorProgrammer)
